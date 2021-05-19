626542839bedf29d7b41929ac5de58b7f3fab6ae
rilez6867 <32180513+rilez6867@users.noreply.github.com>
2021-05-19 07:17:15 -0500

    Update waypoint_updater.py

--- src/waypoint_updater/waypoint_updater.py
+++ src/waypoint_updater/waypoint_updater.py
@@ -8,23 +8,10 @@ from std_msgs.msg import Int32
 
 import math
 
-'''
-This node will publish waypoints from the car's current position to some `x` distance ahead.
 
-As mentioned in the doc, you should ideally first implement a version which does not care
-about traffic lights or obstacles.
 
-Once you have created dbw_node, you will update this node to use the status of traffic lights too.
-
-Please note that our simulator also provides the exact location of traffic lights and their
-current status in `/vehicle/traffic_lights` message. You can use this message to build this node
-as well as to verify your TL classifier.
-
-TODO (for Yousuf and Aaron): Stopline location for each traffic light.
-'''
-
-LOOKAHEAD_WPS = 100# Number of waypoints we will publish. You can change this number
-MAX_DECEL = .5
+LOOKAHEAD_WPS = 50 # Number of waypoints  published. 
+MAX_DECEL = 5
 
 
 class WaypointUpdater(object):
